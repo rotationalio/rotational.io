3a24deac23844414d5814b26456fde5dc69644c4
suyiinang <suyiinang@gmail.com>
2021-04-19 23:23:02 +0800

    update map

--- apps/R/map.R
+++ apps/R/map.R
@@ -7,24 +7,71 @@
 #' @return a \code{shiny::\link[shiny]{tagList}} containing UI elements
 #' 
 
+library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+library(tidyverse)
+library(ggstatsplot)
+library(plotly)
+library(readr)
+library(leaflet)
+library(haven)
+library(funModeling)
+library(crosstalk)
+library(data.table)
+library(ggplot2)
+library(tidytext)
+library(ggfittext)
+library(tidymodels)
+library(ggcorrplot)
+library(vip)
+library(Boruta)
+library(reshape2)
+library(shinycssloaders)
+library(ranger)
+library(skimr)
+library(shinythemes)
+library(rgdal)
+library(sf)
+library(tmap)
+
 final_listings <- read_csv("./data/listing_processed.csv") %>%
   select(-all_of(var_remove)) %>%
   mutate(across(where(is.character), as.factor)) %>%
   mutate(across(where(is.logical), as.factor)) %>%
   mutate(price_per_pax = price/accommodates)
 
-linebreaks <- function(n){HTML(strrep(br(), n))}
+subzone <- readOGR(dsn = "data/spatial", layer="MP14_SUBZONE_WEB_PL")
+airbnb <- select(final_listings, host_is_superhost, review_scores_rating, 
+                 neighbourhood_cleansed, room_type, price_per_pax, latitude, longitude)
+airbnb <- subset(airbnb, !is.na(host_is_superhost))
+airbnb_sf <- st_as_sf(airbnb, coords = c("longitude", "latitude"), crs = 4326)
+
+
+
+mpsz2 <- st_read(dsn = 'data/spatial',
+                 layer = 'MP14_SUBZONE_WEB_PL', 
+                 quiet = TRUE) %>%
+  group_by(PLN_AREA_N) %>%
+  summarise(geometry = sf::st_union(geometry))
+
+# collapse dataframe and summarise price by neighbourhood
+listing_summary <- final_listings %>% 
+  group_by(neighbourhood_cleansed) %>%
+  summarise(avg_price_pp = round(mean(price_per_pax),0),
+            min_price_pp = round(min(price_per_pax),0),
+            max_price_pp = round(max(price_per_pax),0)) %>%
+  mutate_at(.vars = vars(neighbourhood_cleansed), .funs= funs(toupper))
+
+# join by neighbourhood and planning area
+airbnb_map <- right_join(mpsz2,listing_summary, c("PLN_AREA_N" = 'neighbourhood_cleansed'))
+
 
 mapUI <- function(id){
   fluidPage(
-    tags$style(
-      type = 'text/css', 
-      '.bg-orange {background-color: #FF5A5F!important; }'
-    ),
     titlePanel("Mapping Airbnb Singapore"),
     sidebarLayout(
       sidebarPanel(
-        setSliderColor(c("#FF5A60","#FF5A60"), c(1,2)),
         selectInput(inputId = NS(id,'map_type'),
                     label = 'Select map type',
                     choices = c('Point Symbol map',
@@ -32,37 +79,16 @@ mapUI <- function(id){
                     selected = 'Point Symbol map'),
         
         uiOutput(NS(id,'point_view')),
-        
+        uiOutput(NS(id,'show_data')),
         width = 3
-        
       ),
       mainPanel(
-        fluidRow(
-          box(
-            title = "Number of listings",width = 4,background = "orange",
-            'xxx'
-          ),
-          
-          box(
-            title = "Price per person", width = 4, background = "orange",
-            "Average", 
-            br(),
-            "Min",
-            br(),
-            "Max"
-          ),
-          
-          box(
-            title = "Review Scores",width = 4, background = "orange",
-            "Average", 
-            br(),
-            "Min",
-            br(),
-            "Max"),
-          
-        leafletOutput(NS(id,'point_map'), height = 600)
+        leafletOutput(NS(id,'leaf_map'),height = 700),
+        conditionalPanel(
+          condition = "input.map_type == 'Choropleth map'",
+          DT::dataTableOutput(NS(id,'szTable')))
       )
-      )
+      
     )
   )
   
@@ -71,59 +97,119 @@ mapUI <- function(id){
 mapServer <- function(id, final_listings){
   moduleServer(id, function(input, output, session){
     
-    output$point_view <- renderUI({
-      if (input$map_type == "Point Symbol map") {
-        selectInput(inputId = 'filter_point',
-                    label = 'View by',
-                    choices = c('None',
-                                'Host status',
-                                'Room type'),
-                    selected = 'Host status')}
-      else {
-        selectInput(inputId = 'filter_pleth',
-                    label = 'Show subzone by',
-                    choices = c('None',
-                                'Count',
-                                'Price'),
-                    selected = 'Count')
-      }
-    })
-    
-    output$point_map <- renderLeaflet({
+    output$leaf_map <- renderLeaflet({
       
-      subzone <- readOGR(dsn = "data/spatial", layer="MP14_SUBZONE_WEB_PL")
-      
-      airbnb <- select(final_listings, host_is_superhost, review_scores_rating, 
-                       neighbourhood_cleansed, room_type, price_per_pax, latitude, longitude)
-      
-      airbnb_sf <- st_as_sf(airbnb, coords = c("longitude", "latitude"), crs = 4326)
-      
-      sg_host <- levels(final_listings$host_is_superhost)
-      
-      sg_rooms <- levels(final_listings$room_type)
-      pal <- colorFactor(c("plum3", "red","black","blue"), domain = sg_rooms)
-      
-      point_map <- leaflet(airbnb_sf) %>%
+      superhost <- leaflet(airbnb_sf) %>%
         addTiles() %>%
-        addProviderTiles('Esri.WorldGrayCanvas') %>%
+        addProviderTiles('OneMapSG.Original', group = 'Original') %>%
+        addProviderTiles('OneMapSG.Grey', group = 'Grey') %>%
+        addProviderTiles('OneMapSG.Night', group = 'Night') %>%
         addCircleMarkers(
+          data = airbnb[airbnb$host_is_superhost == 'TRUE',],
+          lng = ~longitude,
+          lat = ~latitude,
+          color = '#FF5A5F',
           radius = 2,
-          color = ~pal(room_type),
           stroke = FALSE,
           fillOpacity = 0.5,
-          label = ~round(price_per_pax,2)) %>%
-        addLegend(pal = pal,
-                  values = ~room_type,
-                  opacity = 0.5,
-                  title = 'Airbnb room types',
-                  position = 'bottomright')
-      point_map
+          group = 'Superhost'
+        ) %>%
+        addCircleMarkers(
+          data = airbnb[airbnb$host_is_superhost == 'FALSE',],
+          lng = ~longitude,
+          lat = ~latitude,
+          color = '#00A699',
+          radius = 2,
+          stroke = FALSE,
+          fillOpacity = 0.5,
+          group = 'Regular_host'
+        ) %>%
+        addLayersControl(baseGroups = c('Original', 'Grey', 'Night'),
+                         overlayGroups = c('Superhost','Regular_host'),
+                         options = layersControlOptions(collapsed = FALSE))
+      
+      rooms <- leaflet(airbnb_sf) %>%
+        addTiles() %>%
+        addProviderTiles('OneMapSG.Original', group = 'Original') %>%
+        addProviderTiles('OneMapSG.Grey', group = 'Grey') %>%
+        addProviderTiles('OneMapSG.Night', group = 'Night') %>%
+        addCircleMarkers(
+          data = airbnb[airbnb$room_type == 'Entire home/apt',],
+          lng = ~longitude,
+          lat = ~latitude,
+          color = '#FF5A5F',
+          radius = 2,
+          stroke = FALSE,
+          fillOpacity = 0.5,
+          group = 'Entire home'
+        ) %>%
+        addCircleMarkers(
+          data = airbnb[airbnb$room_type == 'Hotel room',],
+          lng = ~longitude,
+          lat = ~latitude,
+          color = '#00A699',
+          radius = 2,
+          stroke = FALSE,
+          fillOpacity = 0.5,
+          group = 'Hotel room'
+        ) %>%
+        addCircleMarkers(
+          data = airbnb[airbnb$room_type == 'Private room',],
+          lng = ~longitude,
+          lat = ~latitude,
+          color = '#3182bd',
+          radius = 2,
+          stroke = FALSE,
+          fillOpacity = 0.5,
+          group = 'Private room'
+        ) %>%
+        addCircleMarkers(
+          data = airbnb[airbnb$room_type == 'Shared room',],
+          lng = ~longitude,
+          lat = ~latitude,
+          color = '#756bb1',
+          radius = 2,
+          stroke = FALSE,
+          fillOpacity = 0.5,
+          group = 'Shared room'
+        ) %>%  
+        addLayersControl(baseGroups = c('Original', 'Grey', 'Night'),
+                         overlayGroups = c('Entire home','Hotel room', 'Private room','Shared room'),
+                         options = layersControlOptions(collapsed = FALSE))
+      
+      
+      
+      avg_price <- tm_shape(mpsz2)+
+        tm_polygons() +
+        tm_shape(airbnb_map) +
+        tm_fill('avg_price_pp',
+                n = 6,
+                style = 'quantile',
+                palette = 'Blues')+
+        tm_borders(lwd = 0.5, alpha = 1)
+      
+      
+      cho_map <- tmap_leaflet(avg_price) %>%
+        addTiles() %>%
+        addProviderTiles('OneMapSG.Original', group = 'Original') %>%
+        addProviderTiles('OneMapSG.Grey', group = 'Grey') %>%
+        addProviderTiles('OneMapSG.Night', group = 'Night') %>%
+        addLayersControl(baseGroups = c('Original', 'Grey', 'Night'),
+                         options = layersControlOptions(collapsed = FALSE))
+      
+      if(input$map_type == 'Choropleth map'){cho_map}
+      else if(input$filter_point == 'superhost'){ superhost}
+      else if (input$filter_point == 'room_type') {rooms}
+      
     })
     
-    
-    
-    
-    
+    output$szTable <- DT::renderDataTable({
+      if(input$show_data){
+        DT::datatable(data = airbnb %>% select(1:7),
+                      options=list(pageLength = 10),
+                      rownames = FALSE)
+      }
+    })
     
   })
 }
--- apps/app.R
+++ apps/app.R
@@ -24,6 +24,7 @@ library(skimr)
 library(shinythemes)
 library(rgdal)
 library(sf)
+library(tmap)
 
 #########
 var_remove <- c("minimum_minimum_nights", "maximum_minimum_nights",
@@ -35,6 +36,29 @@ final_listings <- read_csv("./data/listing_processed.csv") %>%
     mutate(across(where(is.character), as.factor)) %>%
     mutate(across(where(is.logical), as.factor)) %>%
     mutate(price_per_pax = price/accommodates)
+
+subzone <- readOGR(dsn = "data/spatial", layer="MP14_SUBZONE_WEB_PL")
+airbnb <- select(final_listings, host_is_superhost, review_scores_rating, 
+                 neighbourhood_cleansed, room_type, price_per_pax, latitude, longitude)
+airbnb <- subset(airbnb, !is.na(host_is_superhost))
+airbnb_sf <- st_as_sf(airbnb, coords = c("longitude", "latitude"), crs = 4326)
+
+mpsz2 <- st_read(dsn = 'data/spatial',
+                 layer = 'MP14_SUBZONE_WEB_PL', 
+                 quiet = TRUE) %>%
+    group_by(PLN_AREA_N) %>%
+    summarise(geometry = sf::st_union(geometry))
+
+listing_summary <- final_listings %>% 
+    group_by(neighbourhood_cleansed) %>%
+    summarise(avg_price_pp = round(mean(price_per_pax),0),
+              min_price_pp = round(min(price_per_pax),0),
+              max_price_pp = round(max(price_per_pax),0),
+              avg_review_score = round(mean(review_scores_rating, na.rm = TRUE),0)) %>%
+    mutate_at(.vars = vars(neighbourhood_cleansed), .funs= funs(toupper))
+
+airbnb_map <- right_join(mpsz2,listing_summary, c("PLN_AREA_N" = 'neighbourhood_cleansed'))
+
 
 ########
 
@@ -67,7 +91,30 @@ ui <- dashboardPage(
                                  #put ui here
                         ),
                         tabPanel("Map",
-                                 mapUI('map')
+                                 fluidPage(
+                                     titlePanel("Mapping Airbnb Singapore"),
+                                     sidebarLayout(
+                                         sidebarPanel(
+                                             
+                                             selectInput(inputId = 'map_type',
+                                                         label = 'Select map type',
+                                                         choices = c('Point Symbol map',
+                                                                     'Choropleth map'),
+                                                         selected = 'Point Symbol map'),
+                                             uiOutput('point_view'),
+                                             uiOutput('show_data'),
+                                             width = 2
+                                         ),
+                                         mainPanel(
+                                             leafletOutput('leaf_map', height = 800),
+                                             conditionalPanel(
+                                                 condition = "input.map_type == 'Choropleth map'",
+                                                 DT::dataTableOutput('szTable')
+                                             )
+                                             
+                                         )
+                                     )
+                                 )
                         )
                     ),
             ),
@@ -118,7 +165,133 @@ server <- function(input, output) {
     
     
     ##### map ta b#####
-    mapServer('map', final_listings)
+    output$point_view <- renderUI({
+        if (input$map_type == "Point Symbol map") {
+            selectInput(inputId = 'filter_point',
+                        label = 'View by',
+                        choices = c('Superhost' = 'superhost',
+                                    'Room type' = 'room_type'),
+                        selected = 'Host status')} })
+    
+    output$show_data <- renderUI({
+        if(input$map_type == "Choropleth map"){
+            checkboxInput(inputId = 'show_data',
+                          label = "Show data table",
+                          value = FALSE)
+        }
+    })
+    
+    output$leaf_map <- renderLeaflet({
+        
+        superhost <- leaflet(airbnb_sf) %>%
+            addTiles() %>%
+            addProviderTiles('OneMapSG.Original', group = 'Original') %>%
+            addProviderTiles('OneMapSG.Grey', group = 'Grey') %>%
+            addProviderTiles('OneMapSG.Night', group = 'Night') %>%
+            addCircleMarkers(
+                data = airbnb[airbnb$host_is_superhost == 'TRUE',],
+                lng = ~longitude,
+                lat = ~latitude,
+                color = '#FF5A5F',
+                radius = 2,
+                stroke = FALSE,
+                fillOpacity = 0.5,
+                group = 'Superhost'
+            ) %>%
+            addCircleMarkers(
+                data = airbnb[airbnb$host_is_superhost == 'FALSE',],
+                lng = ~longitude,
+                lat = ~latitude,
+                color = '#00A699',
+                radius = 2,
+                stroke = FALSE,
+                fillOpacity = 0.5,
+                group = 'Regular_host'
+            ) %>%
+            addLayersControl(baseGroups = c('Original', 'Grey', 'Night'),
+                             overlayGroups = c('Superhost','Regular_host'),
+                             options = layersControlOptions(collapsed = FALSE))
+        
+        rooms <- leaflet(airbnb_sf) %>%
+            addTiles() %>%
+            addProviderTiles('OneMapSG.Original', group = 'Original') %>%
+            addProviderTiles('OneMapSG.Grey', group = 'Grey') %>%
+            addProviderTiles('OneMapSG.Night', group = 'Night') %>%
+            addCircleMarkers(
+                data = airbnb[airbnb$room_type == 'Entire home/apt',],
+                lng = ~longitude,
+                lat = ~latitude,
+                color = '#FF5A5F',
+                radius = 2,
+                stroke = FALSE,
+                fillOpacity = 0.5,
+                group = 'Entire home'
+            ) %>%
+            addCircleMarkers(
+                data = airbnb[airbnb$room_type == 'Hotel room',],
+                lng = ~longitude,
+                lat = ~latitude,
+                color = '#00A699',
+                radius = 2,
+                stroke = FALSE,
+                fillOpacity = 0.5,
+                group = 'Hotel room'
+            ) %>%
+            addCircleMarkers(
+                data = airbnb[airbnb$room_type == 'Private room',],
+                lng = ~longitude,
+                lat = ~latitude,
+                color = '#3182bd',
+                radius = 2,
+                stroke = FALSE,
+                fillOpacity = 0.5,
+                group = 'Private room'
+            ) %>%
+            addCircleMarkers(
+                data = airbnb[airbnb$room_type == 'Shared room',],
+                lng = ~longitude,
+                lat = ~latitude,
+                color = '#756bb1',
+                radius = 2,
+                stroke = FALSE,
+                fillOpacity = 0.5,
+                group = 'Shared room'
+            ) %>%  
+            addLayersControl(baseGroups = c('Original', 'Grey', 'Night'),
+                             overlayGroups = c('Entire home','Hotel room', 'Private room','Shared room'),
+                             options = layersControlOptions(collapsed = FALSE))
+        
+        
+        
+        avg_price <- tm_shape(mpsz2)+
+            tm_polygons() +
+            tm_shape(airbnb_map) +
+            tm_fill('avg_price_pp',
+                    n = 6,
+                    style = 'quantile',
+                    palette = 'Blues')+
+            tm_borders(lwd = 0.5, alpha = 1)
+        
+        
+        cho_map <- tmap_leaflet(avg_price) %>%
+            addTiles() %>%
+            addProviderTiles('OneMapSG.Original', group = 'Original') %>%
+            addProviderTiles('OneMapSG.Grey', group = 'Grey') %>%
+            addProviderTiles('OneMapSG.Night', group = 'Night') %>%
+            addLayersControl(baseGroups = c('Original', 'Grey', 'Night'),
+                             options = layersControlOptions(collapsed = FALSE))
+        
+        if(input$map_type == 'Choropleth map'){cho_map}
+        else if(input$filter_point == 'superhost'){ superhost}
+        else if (input$filter_point == 'room_type') {rooms}
+        
+    })
+    
+    output$szTable <- DT::renderDataTable({
+        if(input$show_data){
+            DT::datatable(data = listing_summary)
+        }
+    })
     
     
     
