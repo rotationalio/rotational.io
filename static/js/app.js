8f0c0354534c875dc164336c5d26468b4186cbb2
Cletus Razakou <masskoder@gmail.com>
2022-10-14 12:32:06 -0400

    clean up

--- static/js/app.js
+++ static/js/app.js
@@ -38,6 +38,9 @@ function myFunction() {
         return response.json()
 
       })
+      .then((data) => {
+        console.log('Success:', data);
+      })
      
       .catch((error) => {
         console.error('Error:', error);
