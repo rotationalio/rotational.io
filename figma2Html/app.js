0edfeb798bf8e2840fa190f3609c2c0e0f77690e
Cletus Razakou <masskoder@gmail.com>
2022-10-14 11:35:35 -0400

    minor fix on contact form (#86)
    * update contact js file
    
    * reset the form
    
    * move integrated html file to new folder

---
+++ figma2Html/app.js
@@ -0,0 +1,10 @@
+// public
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
+
