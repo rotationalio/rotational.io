9de04924f4b73f1fde2840cb9bcb91861125ff70
daniellemaxwell <daniellemaxwell@users.noreply.github.com>
2022-10-17 15:37:55 +0000

    deploy: e2714a8304f036ff98014da5689fa0eea0d72cb0

--- js/app.js
+++ js/app.js
@@ -7,44 +7,85 @@ function myFunction() {
       x.className = "topnav";
     }
 }
-  const form = document.getElementById('contactForm');
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData);
-    const { subscribe, ...rest } = data;
+const form = document.getElementById('contactForm');
 
-    const formattedData = {
+form?.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  const { subscribe, ...rest } = data;
+
+  const formattedData = {
       
-      subscribe: subscribe === 'on' ? true : false,
-      ...rest,
-      lists: [
-        "4ada7d4b-e0a7-4017-8b9d-4db172b5be64",
-        "54b7fc6a-db4b-491b-b6ff-4348c15072bc"
+    subscribe: subscribe === 'on' ? true : false,
+    ...rest,
+    lists: [
+      "4ada7d4b-e0a7-4017-8b9d-4db172b5be64",
+      "54b7fc6a-db4b-491b-b6ff-4348c15072bc"
     ]
-    }
-    fetch('https://api.rotationallabs.com/v1/contact', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formattedData),
-    })
-      .then((response) => {
-        if (response.status === 204) {
-          document.getElementById('contact-alert').style.display = 'block';
-          form.reset();
-        } 
-        return response.json()
-
-      })
-      .then((data) => {
-        console.log('Success:', data);
-      })
-     
-      .catch((error) => {
-        console.error('Error:', error);
-      });
   }
-  );
-  
+  fetch('https://api.rotationallabs.com/v1/contact', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formattedData),
+  })
+    .then((response) => {
+      if (response.status === 204) {
+        document.getElementById('contact-alert').style.display = 'block';
+        form.reset();
+      }
+      return response.json()
+
+    })
+    .then((data) => {
+      console.log('Success:', data);
+    })
+     
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+}
+);
+
+// check newsletter form submission
+const newsletterForm = document.getElementById('newsletterForm');
+newsletterForm?.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('newsletter form submitted');
+  const formData = new FormData(newsletterForm);
+  const data = Object.fromEntries(formData);
+
+
+  const formattedData = {
+      
+    ...data,
+    lists: [
+      "4ada7d4b-e0a7-4017-8b9d-4db172b5be64",
+      "54b7fc6a-db4b-491b-b6ff-4348c15072bc"
+    ]
+  }
+  fetch('https://api.rotationallabs.com/v1/notifications/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formattedData),
+  })
+     .then((response) => {
+      if (response.status === 204) {
+        document.getElementById('newsletter-alert').style.display = 'block';
+        form.reset();
+      }
+      return response.json()
+
+    })
+    .then((data) => {
+      console.log('Success:', data);
+    })
+     
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+});
