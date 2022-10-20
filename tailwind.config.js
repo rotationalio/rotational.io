ba3b7565243e893090f77c7a6f21f2763c8128fa
Cletus Razakou <masskoder@gmail.com>
2022-10-20 09:37:06 -0400

    update header and footer image ratio (#95)
    * refactor: update header ratio image
    
    * refactor: apply aspect ratio in footer & handle responsive
    
    * refact: improve wave responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
       },
       backgroundImage: {
         "hero-pattern": "url('/img/hero.png')",
+        'hero-footer': "url('/img/foot.png')",
+        "wave-pattern": "url('/img/wave.png')",
+        "wavexl-pattern": "url('/img/wave_xl.png')",
+        "wavelg-pattern": "url('/img/wave_lg.png')",
+        
+      },
+      backgroundPosition: {
+        'right-center': 'right center',
       },
     },
   },
