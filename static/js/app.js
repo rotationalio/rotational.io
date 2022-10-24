f97a4dc04640b8e375baa9418fc55823b72733f8
Cletus Razakou <masskoder@gmail.com>
2022-10-24 12:23:10 -0400

    Update twitter blog post feature (#101)
    * feat: added share blog post feature
    
    * style: clean up element space
    
    * style: clean up
    
    * feat: added og tag for better SEO experience
    
    * refact: added absolute url for og:image
    
    * refact: use twitter web intent to share blog post
    
    * refact: clean up & checkpoint
    
    * remove javascript void

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 // static
-function myFunction() {
+function openMobNav() {
   var x = document.getElementById('myTopnav');
   if (x.className === 'topnav') {
     x.className += ' responsive';
@@ -46,7 +46,7 @@ form?.addEventListener('submit', (event) => {
     });
 });
 
-// share on twitter with title , description and url
+// share on twitter 
 
 function shareOnTwitter() {
   const twitterWindow = window.open(
@@ -58,6 +58,25 @@ function shareOnTwitter() {
     twitterWindow.focus();
   }
   return false;
+}
+
+// share on tiwtter with title , url
+function shareOnTwitterWithTitle() {
+  const title = document.querySelector('[data-blog-title]').innerText;
+  const twitterWindow = window.open(
+    'https://twitter.com/intent/tweet?text=' +
+    title +
+    '&url=' +
+    document.URL +
+    '&via=rotationallabs',
+    'twitter-popup',
+    'height=350,width=600'
+  );
+  if (twitterWindow.focus) {
+    twitterWindow.focus();
+  }
+  return false;
+
 }
 
 //share on linkedin
@@ -86,29 +105,6 @@ function shareByEmail() {
   return false;
 }
 
-
-
-// share on twitter
-function shareOnTwitter() {
-  const twitterWindow = window.open('https://twitter.com/share?url=' + document.URL, 'twitter-popup', 'height=350,width=600');  
-  if (twitterWindow.focus) { twitterWindow.focus(); }
-  return false;
-}
-
-//share on linkedin
-function shareOnLinkedIn() {
-  const linkedinWindow = window.open('https://www.linkedin.com/shareArticle?mini=true&url=' + document.URL, 'linkedin-popup', 'height=350,width=600');
-  if (linkedinWindow.focus) { linkedinWindow.focus(); }
-  return false;
-
-}
-
-// share by email 
-function shareByEmail() {
-  const emailWindow = window.open('mailto:?subject=Check out this article&body=' + document.URL, 'email-popup', 'height=350,width=600');
-  if (emailWindow.focus) { emailWindow.focus(); }
-  return false;
-}
 
 
 // check newsletter form submission
