4c84cfe0946513a102638298f6bfeef6f22f8f3e
Danielle <danielle@rotational.io>
2024-09-11 12:52:42 -0400

    Display testimonials in carousel (#378)
    Co-authored-by: Benjamin Bengfort <benjamin@bengfort.com>

--- static/js/app.js
+++ static/js/app.js
@@ -412,3 +412,16 @@ downloadPlaybookBttn?.addEventListener('click', () => {
     downloadDropdown.classList.add('hidden');
   });
 })
+
+// Initialize Embla carousel
+const emblaRoot = document.querySelector('.embla');
+const emblaViewport = emblaRoot?.querySelector('.embla__viewport');
+const prevBtn = emblaRoot?.querySelector('.embla__prev');
+const nextBtn = emblaRoot?.querySelector('.embla__next');
+const options = { loop: true };
+
+if (emblaRoot) {
+  const carousel = EmblaCarousel(emblaViewport, options);
+  prevBtn.addEventListener('click', carousel.scrollPrev, false);
+  nextBtn.addEventListener('click', carousel.scrollNext, false);
+}
