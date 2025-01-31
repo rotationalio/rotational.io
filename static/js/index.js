a2014df177da684d2f4ecb7fa62a724d2d288246
daniellemaxwell <danielle@rotational.io>
2025-01-31 15:57:05 -0500

    Add logos and display in carousel

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,16 @@
-// Initialize Embla carousel
-const emblaRoot = document.querySelector('.embla');
-const emblaViewport = emblaRoot?.querySelector('.embla__viewport');
-const prevBtn = emblaRoot?.querySelector('.embla__prev');
-const nextBtn = emblaRoot?.querySelector('.embla__next');
-const options = { loop: true };
+// Initialize Embla carousel. 
+const emblaRoot = document.querySelectorAll('.embla');
 
 if (emblaRoot) {
-  const carousel = EmblaCarousel(emblaViewport, options);
-  prevBtn.addEventListener('click', carousel.scrollPrev, false);
-  nextBtn.addEventListener('click', carousel.scrollNext, false);
-}
+  emblaRoot?.forEach((root) => {
+    // Set carousel options from the data attributes.
+   const options = { loop: Boolean(root?.dataset?.loop), slidesToScroll: Number(root?.dataset?.scroll)};
+    const carousel = EmblaCarousel(root?.querySelector('.embla__viewport'), options);
+
+    const prevBtn = root?.querySelector('.embla__prev');
+    const nextBtn = root?.querySelector('.embla__next');
+
+    prevBtn?.addEventListener('click', carousel?.scrollPrev, false);
+    nextBtn?.addEventListener('click', carousel?.scrollNext, false);
+  });
+};
