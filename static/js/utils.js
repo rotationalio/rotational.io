11ba0a6638205fe2a96895dce086cd03f23b6407
daniellemaxwell <danielle@rotational.io>
2025-03-24 11:48:30 -0400

    Replace regex with functions in MDN docs to get cookie

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,4 +1,4 @@
 // Get the HubSpot tracking cookie from the browser to be included in the form submission data.
 export function getHubspotCookie() {
-  return document.cookie.replace(/(?:(?:^|.*;\s*)hubspotutk\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  return document.cookie.split("; ").find((row) => row.startsWith("hubspotutk="))?.split("=")[1];
 }
